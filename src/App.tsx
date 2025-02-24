ab39297005d365c9df680da0d3da7f8a387e9379
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-02-24 13:23:50 +0100

    feat: make type acquisition work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./global.d.ts";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
-import Editor from "@monaco-editor/react";
+import Editor, { OnChange, OnMount } from "@monaco-editor/react";
 import { type ZWaveSerialBindingFactory } from "@zwave-js/serial";
 import { createWebSerialPortFactory } from "@zwave-js/bindings-browser/serial";
 import "./setimmediate.js";
+import { setupTypeAcquisition } from "@typescript/ata";
+
+// FIXME: There should be a way to reuse the TS instance from the editor
+import ts from "typescript";
 
 interface AppProps {
   esbuild: typeof import("esbuild-wasm");
@@ -26,6 +30,16 @@ const external = [
   "@zwave-js/core/bindings/log/node",
   "node:crypto",
   // "source-map-support",
+];
+
+const typesFilter = [
+  "zwave-js",
+  "@zwave-js/shared",
+  "@zwave-js/core",
+  "@zwave-js/cc",
+  "@zwave-js/config",
+  "@zwave-js/nvmedit",
+  "@zwave-js/serial",
 ];
 
 async function getPort(): Promise<{
@@ -98,12 +112,14 @@ driver.once("driver ready", () => {
 
 // Start the driver
 await driver.start();
-`.trim()
+    `.trim()
   );
+
+  const ataRef = useRef<ReturnType<typeof setupTypeAcquisition>>(null);
+  const debounceTimeoutRef = useRef<number | null>(null);
 
   const handleRunClick = async () => {
     try {
-      // Plugin zum Patchen von Importen
       const patchImportsPlugin = {
         name: "patch-imports",
         setup(build: any) {
@@ -140,8 +156,13 @@ await driver.start();
         },
       });
 
+      const actualCode = `
+const Buffer = (await import("@zwave-js/shared")).Bytes;
+${result.outputFiles[0].text}
+`;
+
       // Code in Blob konvertieren und als Modul ausführen
-      const blob = new Blob([result.outputFiles[0].text], {
+      const blob = new Blob([actualCode], {
         type: "text/javascript",
       });
       const url = URL.createObjectURL(blob);
@@ -150,25 +171,173 @@ await driver.start();
       const originalDefine = window.define;
       window.define = undefined;
 
-      // @ts-expect-error
-      window.Bytes ??= (await import("@zwave-js/shared")).Bytes;
-
       // Ensure the script has access to the serial port
       if (!window.port || !window.serialBinding) {
         await getPort();
       }
 
+      window.Bytes ??= (await import("@zwave-js/shared")).Bytes;
+      window.Buffer = window.Bytes;
+
       try {
-        /* @vite-ignore */
-        await import(url);
+        await import(/* @vite-ignore */ url);
       } finally {
         URL.revokeObjectURL(url);
         window.define = originalDefine;
       }
-    } catch (error) {
+    } catch (error: any) {
       alert(error.message + "\n" + error.stack);
       console.error(error);
     }
+  };
+
+  const handleEditorDidMount: OnMount = async (editor, monaco) => {
+    const defaults = monaco.languages.typescript.typescriptDefaults;
+
+    defaults.setCompilerOptions({
+      module: monaco.languages.typescript.ModuleKind.ESNext,
+      target: monaco.languages.typescript.ScriptTarget.ESNext,
+    });
+
+    // Bytes/Buffer are globally available
+    defaults.addExtraLib(
+      `
+declare const Bytes: typeof import("@zwave-js/shared").Bytes;
+declare const Buffer: typeof Bytes;
+`,
+      "playground_global.d.ts"
+    );
+
+    // For some reason, simply loading the type definitions does not work, so we set up tsconfig paths instead
+    const entrypoints = new Map<string, string>();
+
+    ataRef.current = setupTypeAcquisition({
+      projectName: "Z-Wave JS Playground",
+      typescript: ts,
+      logger: console,
+      async fetcher(input: string, init) {
+        // Filter out requests for external modules
+        const moduleName = /\/npm\/(.+?)@/.exec(input)?.[1];
+        if (!moduleName || !typesFilter.includes(moduleName)) {
+          console.warn("filtered request for ", input);
+          return new Response(null, {
+            status: 404,
+            statusText: "Not Found",
+          });
+        }
+
+        // Modify the /resolve JSDelivr API response to return the embedded version for our packages
+        if (
+          input.includes("/resolve/npm/") &&
+          (input.includes("/zwave-js") || input.includes("/@zwave-js")) &&
+          input.endsWith("@latest")
+        ) {
+          return new Response(
+            JSON.stringify({
+              version: "14.3.8-0-pr-7581-401b2ca",
+            })
+          );
+        }
+
+        // Modify the /flat JSDelivr API response to omit CommonJS files
+        if (input.endsWith("/flat")) {
+          const response = await fetch(input, init);
+          const flat = await response.json();
+          flat.files = flat.files.filter(
+            (file: any) => !file.name.includes("/build/cjs/")
+          );
+          return new Response(JSON.stringify(flat));
+        }
+
+        return fetch(input, init);
+      },
+      delegate: {
+        receivedFile: (code: string, path: string) => {
+          // Monaco editor has no support for package.json exports, so we fake the resolution ourselves
+          if (path.endsWith("package.json")) {
+            const packageJson = JSON.parse(code);
+            if (
+              typeof packageJson.exports === "object" &&
+              packageJson.exports["."] &&
+              !packageJson.types
+            ) {
+              for (const [key, exprt] of Object.entries(packageJson.exports)) {
+                if (typeof exprt !== "object" || exprt === null) continue;
+                if (!key.startsWith(".")) continue;
+
+                // Remember which files are an entrypoint to a module
+                for (const [mode, subpath] of Object.entries(exprt)) {
+                  if (
+                    mode === "browser" ||
+                    mode === "import" ||
+                    mode === "default"
+                  ) {
+                    entrypoints.set(
+                      `/node_modules/${(subpath as string).replace(
+                        /^\./,
+                        packageJson.name
+                      )}`.replace(/\.js$/, ".d.ts"),
+                      key.replace(/^\./, packageJson.name)
+                    );
+                    break;
+                  }
+                }
+              }
+            }
+            // No need to add the package.json to the VFS
+            return;
+          }
+
+          const addLib = (code: string, path: string) => {
+            monaco.languages.typescript.typescriptDefaults.addExtraLib(
+              code,
+              path
+            );
+            // Creating the models allows goto definition,
+            // but this also causes the editor to throw "leak" warnings
+            // const uri = monaco.Uri.file(path);
+            // if (monaco.editor.getModel(uri) === null) {
+            //   monaco.editor.createModel(code, "javascript", uri);
+            // }
+          };
+
+          addLib(code, path);
+        },
+        // started: () => {
+        //   console.log("ATA start");
+        // },
+        // progress: (downloaded: number, total: number) => {
+        //   console.log(`Got ${downloaded} out of ${total}`);
+        // },
+        finished: (vfs) => {
+          // console.log("ATA done");
+
+          // Update the compiler options with the new paths
+          const compilerOptions = defaults.getCompilerOptions();
+          for (const [path, mod] of entrypoints) {
+            compilerOptions.paths ??= {};
+            compilerOptions.paths[mod] = [path];
+          }
+          defaults.setCompilerOptions(compilerOptions);
+        },
+      },
+    });
+
+    // No matter the code, we always need to import the shared package for `Bytes/Buffer` to be globally available
+    await ataRef.current(`import "@zwave-js/shared"`);
+    await ataRef.current(code);
+  };
+
+  const handleEditorChange: OnChange = (value) => {
+    setCode(value || "");
+
+    // Perform type acquisition, but make sure it does not happen too often
+    if (debounceTimeoutRef.current) {
+      clearTimeout(debounceTimeoutRef.current);
+    }
+    debounceTimeoutRef.current = setTimeout(async () => {
+      await ataRef.current?.(code);
+    }, 2500);
   };
 
   return (
@@ -178,7 +347,10 @@ await driver.start();
         theme="vs-dark"
         defaultLanguage="typescript"
         defaultValue={code}
-        onChange={(value) => setCode(value || "")}
+        onChange={handleEditorChange}
+        onMount={handleEditorDidMount}
+        defaultPath="script.ts"
+        path="script.ts"
       />
 
       <button id="run" onClick={handleRunClick}>
--- src/global.d.ts
+++ src/global.d.ts
@@ -2,4 +2,6 @@ interface Window {
   define?: Function;
   port?: SerialPort;
   serialBinding?: ZWaveSerialBindingFactory;
+  Bytes: (typeof import("@zwave-js/shared").Bytes);
+  Buffer: (typeof import("@zwave-js/shared").Bytes);
 }
