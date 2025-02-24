2e08f9fea0fe613eceef9ae635802c58b8670fa7
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-02-24 13:26:27 +0100

    refactor: import default code at compile time

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { type ZWaveSerialBindingFactory } from "@zwave-js/serial";
 import { createWebSerialPortFactory } from "@zwave-js/bindings-browser/serial";
 import "./setimmediate.js";
 import { setupTypeAcquisition } from "@typescript/ata";
+import defaultCode from "./assets/default.ts?raw";
 
 // FIXME: There should be a way to reuse the TS instance from the editor
 import ts from "typescript";
@@ -68,52 +69,7 @@ async function getPort(): Promise<{
 }
 
 function App({ esbuild }: AppProps) {
-  const [code, setCode] = useState(
-    `
-import { Driver } from "zwave-js";
-
-const driver = new Driver(
-    // Tell the driver which serial port to use
-    "/dev/serial/by-id/my-usb-port",
-    // and configure options like security keys
-    {
-        securityKeys: {
-            S0_Legacy: Bytes.from("0102030405060708090a0b0c0d0e0f10", "hex"),
-            S2_Unauthenticated: Bytes.from(
-                "11111111111111111111111111111111",
-                "hex",
-            ),
-            S2_AccessControl: Bytes.from(
-                "22222222222222222222222222222222",
-                "hex",
-            ),
-            S2_Authenticated: Bytes.from(
-                "33333333333333333333333333333333",
-                "hex",
-            ),
-        },
-        securityKeysLongRange: {
-            S2_Authenticated: Bytes.from(
-                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-                "hex",
-            ),
-            S2_AccessControl: Bytes.from(
-                "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-                "hex",
-            ),
-        },
-    },
-);
-
-// Listen for the driver ready event before doing anything with the driver
-driver.once("driver ready", () => {
-    console.log("Driver is ready");
-});
-
-// Start the driver
-await driver.start();
-    `.trim()
-  );
+  const [code, setCode] = useState(defaultCode.trim());
 
   const ataRef = useRef<ReturnType<typeof setupTypeAcquisition>>(null);
   const debounceTimeoutRef = useRef<number | null>(null);
