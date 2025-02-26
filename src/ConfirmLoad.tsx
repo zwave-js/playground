c7838c88b7f16bf20ea873a58543c1824ef6eecc
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-02-26 10:21:09 +0100

    feat: mention local processing in loader confirmation

--- src/ConfirmLoad.tsx
+++ src/ConfirmLoad.tsx
@@ -15,12 +15,10 @@ export const ConfirmLoad = () => {
         style={{ margin: "1.25em 0" }}
       />
 
-      <div>
-        To enable the Z-Wave JS Playground, click here.
-        <br />
-        <span style={{ fontWeight: "normal", fontSize: "85%" }}>
-          Note that this will download about 30 megabytes of data.
-        </span>
+      <div className="loader-text">
+        <span>To enable the Z-Wave JS Playground, click here</span>
+        <span>Note that this will download about 30 megabytes of data.</span>
+        <span>Everything will be processed locally in your browser.</span>
       </div>
     </button>
   );
